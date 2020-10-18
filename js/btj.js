fd9419a433f2d532f7937ac4163859f7148942b5
LuJZ1402 <72901618+LuJZ1402@users.noreply.github.com>
2020-10-18 18:37:26 +0800

    删除 btj.js

--- js/btj.js
+++
@@ -1,7 +0,0 @@
-﻿var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "//hm.baidu.com/hm.js?05bde3ad80f2ae0330d9f88900f3e4e1";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
